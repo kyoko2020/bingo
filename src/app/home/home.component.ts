5f5a4847f45afdec032953ec83851a13ab504b51
Hayashi Kyoko <hayashi.kyoko@neut.co.jp>
2020-10-16 18:33:52 +0900

    10/16ランダムに数字を回す

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
    delArrayNum: number = 0;
-   targetNumber: number[] = new Array();
+   targetNumber: string[] = new Array();
    winningNumber: string[] =new Array();
    btnName: string = "START";
    panel1: string;
@@ -22,12 +22,16 @@ export class HomeComponent implements OnInit {
    targetIndex: string;
    
    
+   
   ngOnInit():void {
 
     //1-75を配列に追加
-    for(let i: number = 0; i <= MAX_NUMBER; i++){
-    this.targetNumber.push(i);
-    }
+  this.targetNumber = (function(from, to) {
+      for(var tmp = []; from <= to; from++) {
+          tmp.push(('00' + from).slice(-2));
+          }
+          return tmp;
+      })(1, 75);
 
     this.panel1 = "0";
     this.panel10 = "0";
@@ -38,35 +42,33 @@ export class HomeComponent implements OnInit {
 
     if(this.btnName === "START"){
       this.timerId = setInterval( () => {
-        var noDigitNumder = Math.floor(Math.random() * this.targetNumber.length).toString(); 
+        var onetargetIndex = Math.floor(Math.random() * this.targetNumber.length).toString(); 
         // timerId.textContent = this.targetNumber[targetIndex];        
-        //2桁にし、05表示をする
-         this.targetIndex =("00"+ noDigitNumder).slice(-2);
-        
-     ;
 
-        if(this.targetIndex != "00"){
+        this.targetIndex = ( '00'  + onetargetIndex ).slice( -2 );
         //ランダムで表示されている数字を1のくらいと、10のくらいで分割して表示させる
         this.panel1 = this.targetIndex.substr(0,1); 
         this.panel10 =this.targetIndex.substr(1,2); 
-        }else{
 
-        }
-
-            //表示用に配列に保持
-         this.winningNumber.push(this.targetIndex)
-         
         //ランダムで選択された数字targetIndexをtargetNumber配列から選定
-        this.delArrayNum = this.targetNumber.indexOf(Number(this.targetIndex));
-          if(this.delArrayNum >= 0){
-            //targetNumber配列から削除
-            this.targetNumber.splice(this.delArrayNum,1);
-          }
+        this.delArrayNum = this.targetNumber.indexOf(this.targetIndex);
+
         this.btnName = "STOP";
        
         return;
       },10)
     }else if(this.btnName === "STOP"){
+ 
+      if(this.delArrayNum >= 0){
+
+        this.winningNumber.push(this.targetIndex);
+        // if(this.winningNumber.length > 1){
+        // //表示用に配列に保持
+        // this.winningNumber.shift();
+        // }
+        //targetNumber配列から削除
+        this.targetNumber.splice(this.delArrayNum,1);
+      }
       clearInterval(this.timerId);
       // let targetIndex = this.targetNumber.indexOf(Number(targetIndex));   
       // this.targetNumber.splice(this.targetIndex, 1);
@@ -84,7 +86,7 @@ export class HomeComponent implements OnInit {
   }
 
   delBtn(): void{
-    var display =confirm("抽選結果をすべてクリアします。よろしいですか?"); // 警告ダイアログを表示
+    var display = confirm("抽選結果をすべてクリアします。よろしいですか?"); // 警告ダイアログを表示
     if(display){
       //削除成功,データを消す
       clearInterval();
