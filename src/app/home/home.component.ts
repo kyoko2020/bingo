81bd3a2237a718ebeb416c24c2ea472744b779c2
Hayashi Kyoko <hayashi.kyoko@neut.co.jp>
2020-10-21 14:59:29 +0900

    10/21ランダム数字完成

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
- const MAX_NUMBER = 75;
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,87 +9,94 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit {
 
+   // tslint:disable-next-line: no-inferrable-types
    delArrayNum: number = 0;
    targetNumber: string[] = new Array();
-   winningNumber: string[] =new Array();
-   btnName: string = "START";
+   winningNumber: string[] = new Array();
+   isStartFlg: boolean;
    panel1: string;
    panel10: string;
-   timerId =null;
+   timerId = null;
    targetIndex: string;
-   
-   
-   
-  ngOnInit():void {
+   isPush: boolean;
 
-    //1-75を配列に追加
-  this.targetNumber = (function(from, to) {
-      for(var tmp = []; from <= to; from++) {
-          tmp.push(('00' + from).slice(-2));
-          }
-          return tmp;
-      })(1, 75);
+  ngOnInit(): void {
 
-    this.panel1 = "0";
-    this.panel10 = "0";
+    // 1-75を配列に追加
 
+  for (let i = 0; i < 5; i++) {
+    this.targetNumber.push(('00' + (1 + i)).slice(-2));
   }
 
-  staBtn():void{
+  this.panel1 = '0';
+  this.panel10 = '0';
 
-    if(this.btnName === "START"){
+  this.isStartFlg =  true;
+  }
+
+  staBtn(): void{
+
+    // todo 最後一つだけだと回らない
+
+    if (this.isStartFlg){
       this.timerId = setInterval( () => {
-        var onetargetIndex = Math.floor(Math.random() * this.targetNumber.length).toString(); 
-        // timerId.textContent = this.targetNumber[targetIndex];        
-
+        const onetargetIndex = this.targetNumber[Math.floor(Math.random() * this.targetNumber.length)];
         this.targetIndex = ( '00'  + onetargetIndex ).slice( -2 );
-        //ランダムで表示されている数字を1のくらいと、10のくらいで分割して表示させる
-        this.panel1 = this.targetIndex.substr(0,1); 
-        this.panel10 =this.targetIndex.substr(1,2); 
 
-        //ランダムで選択された数字targetIndexをtargetNumber配列から選定
+        // ランダムで表示されている数字を1のくらいと、10のくらいで分割して表示させる
+        this.panel1 = this.targetIndex.substr(0, 1);
+        this.panel10 = this.targetIndex.substr(1, 2);
+
+        // ランダムで選択された数字targetIndexをtargetNumber配列から選定
         this.delArrayNum = this.targetNumber.indexOf(this.targetIndex);
-
-        this.btnName = "STOP";
-       
+        this.isStartFlg = false;
         return;
-      },10)
-    }else if(this.btnName === "STOP"){
- 
-      if(this.delArrayNum >= 0){
-
+      }, 10);
+    }else{
+      if (this.targetNumber != null){
         this.winningNumber.push(this.targetIndex);
-        // if(this.winningNumber.length > 1){
-        // //表示用に配列に保持
-        // this.winningNumber.shift();
-        // }
-        //targetNumber配列から削除
-        this.targetNumber.splice(this.delArrayNum,1);
+        this.isPush = false;
       }
+      console.log(this.winningNumber);
+
+      if (this.delArrayNum >= 0){
+
+        // targetNumber配列から削除
+        this.targetNumber.splice(this.delArrayNum, 1);
+
+        console.log(this.targetNumber);
+
+      }
+
       clearInterval(this.timerId);
-      // let targetIndex = this.targetNumber.indexOf(Number(targetIndex));   
+      // let targetIndex = this.targetNumber.indexOf(Number(targetIndex));
       // this.targetNumber.splice(this.targetIndex, 1);
-      this.btnName = "START";
+
+      this.isStartFlg = true;
       return;
-    
     }
 
-    //数字がなくなったら発表を終わる
-    if(this.targetNumber.length === 0){
+    // 数字がなくなったら発表を終わる
+    if (this.targetNumber.length === 0){
       alert('すべての番号の発表が終わりました') ;
-      clearInterval(this.timerId);
+      this.winningNumber = [];
+      this.isPush = true;
       return;
     }
   }
 
   delBtn(): void{
-    var display = confirm("抽選結果をすべてクリアします。よろしいですか?"); // 警告ダイアログを表示
-    if(display){
-      //削除成功,データを消す
-      clearInterval();
+    const display = confirm('抽選結果をすべてクリアします。よろしいですか?'); // 警告ダイアログを表示
+    if (display){
+      // 削除成功,データを消す
+      // todo 数字が回っている時
+      this.winningNumber = [];
+      this.panel1 = '0';
+      this.panel10 = '0';
+
     }else{
-      //削除キャンセル
-      window.alert('キャンセルされました'); // 警告ダイアログを表示 
+      // 削除キャンセル
+      window.alert('キャンセルされました');
     }
   }
 }
